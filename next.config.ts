22c56d4dd62f3c40a862711ef1b1af051d6a29c3
SHUBH-GUPTA1 <98265659+SHUBH-GUPTA1@users.noreply.github.com>
2025-03-16 16:56:47 +0530

    Disabled ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   devIndicators: false
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+
+  eslint: {
+    ignoreDuringBuilds: true, // This will disable ESLint errors during the build process
+  },
 };
 
 export default nextConfig;
